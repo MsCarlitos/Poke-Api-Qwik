cd8d304838b36e1de63226b350635b9f836aa190
MsCarlitos <Camendozacontreras5@gmail.com>
2023-06-24 16:04:04 -0700

    Formularios, Validaciones, autenticación, Layouts y Rutas

---
+++ src/routes/dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { component$ } from '@builder.io/qwik';
+
+export default component$(() => {
+  return (
+    <>
+        <h3>Admin Dashboard</h3>
+        <p>Esta rura debe de ser privada</p>
+    </>
+  )
+});
